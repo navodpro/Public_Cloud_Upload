d8275233ef3aa0cbf577e35fcc8f626f0cb4a5d5
navodpro <66838043+navodpro@users.noreply.github.com>
2021-03-15 06:48:19 -0700

    Update cmd.py

--- bot/plugins/cmd/cmd.py
+++ bot/plugins/cmd/cmd.py
@@ -11,7 +11,7 @@ from bot import Msg
 async def start_message(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{Msg.start}{Msg.source}",
+        text=f"Hey {message.from_user.first_name}\n Send Me Any Telegram File.\n Choose A Cloud You That Want To Upload.\nI Will Upload Your File To The Coud And Send You a Shareable Link.\n{Msg.start}\n{Msg.source}",
         reply_to_message_id=message.message_id,
         parse_mode="html"
     )
@@ -19,7 +19,7 @@ async def start_message(client, message):
 async def help_message(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{Msg.help}{Msg.source}",
+        text=f"Hey {message.from_user.first_name}\n Send Me Any Telegram File.\n Choose A Cloud You That Want To Upload.\nI Will Upload Your File To The Coud And Send You a Shareable Link.\n{Msg.help}\n{Msg.source} ",
         reply_to_message_id=message.message_id,
         parse_mode="html"
     )
